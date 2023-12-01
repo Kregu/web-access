b138acc397a37407de5ee12a970a899dcee011aa
Sergey Gorokhov <sergey.login@gmail.com>
2023-12-01 21:16:58 +0300

    add transform to json

--- main.go
+++ main.go
@@ -1,37 +1,58 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 )
+
+type Router struct {
+	Model string
+	Wan   string
+}
 
 var index = template.Must(template.ParseFiles("index.html"))
 var form = template.Must(template.ParseFiles("form.html"))
 
 func indexHandler(writer http.ResponseWriter, request *http.Request) {
-    if request.Method == http.MethodGet {
-        index.Execute(writer, nil)
-    } else if request.Method == http.MethodPost {
-        request.ParseForm()
-        data1 := request.Form["router"][0]
-        data2 := request.Form["wan"][0]
-        data3 := data1 + " " + data2
-        form.Execute(writer, data3)
-    }
+	if request.Method == http.MethodGet {
+		index.Execute(writer, nil)
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		router := request.Form["router"][0]
+		wan := request.Form["wan"][0]
+		data := router + " " + wan
+
+		r1 := Router{
+			Model: router,
+			Wan:   wan,
+		}
+		json_data, err := json.Marshal(r1)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(json_data))
+		form.Execute(writer, data)
+	}
+
 }
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "3000"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
 
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    fs := http.FileServer(http.Dir("assets"))
-    mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-    mux.HandleFunc("/", indexHandler)
-    http.ListenAndServe(":"+port, mux)
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
 }
